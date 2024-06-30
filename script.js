f2844bd5c610131fa1db82b43592165c63ab4e30
salonikumari1234 <salonikumari_23cs368@dtu.ac.in>
2024-06-30 09:22:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00D7";
+        li.appendChild(span);
+        saveData();
+    }
+    inputBox.value = "";
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+    // Reattach event listeners to each list item and its close button
+    const listItems = listContainer.querySelectorAll("li");
+    listItems.forEach(item => {
+        item.querySelector("span").addEventListener("click", function() {
+            item.remove();
+            saveData();
+        });
+        item.addEventListener("click", function() {
+            item.classList.toggle("checked");
+            saveData();
+        });
+    });
+}
+
+showTask();
